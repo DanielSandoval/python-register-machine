09c470b12b38c9ae3272345c4416410bf4646f52
Daniel Sandoval <danielsandoval182@gmail.com>
2015-05-13 10:33:21 -0600

    With functions

--- application.py
+++ application.py
@@ -2,15 +2,38 @@
 # Aqui escribe tu codigo
 
 import os
+import sys
+
+MY_PRODUCTS = {} #This is a dictionary to have all product we have on sale
+EXISTENCE_PRODUCT = [] #This a list to have the products we have sold
+PRODUCTS_SOLD = [] #We make a list to save the products we want
+MY_OPTION = [] #It is a list to save the option gold/siver/done
+
 
 def clearscreen():
     """Creates a function for clean the screen"""
     os.system('reset')
 
-#--------------------------OPCION 1--------------------------
-
+'''-------------------------OPCION 1-------------------------'''
 def option1():
     """The option number 1 of the Main Menu"""
+    clearscreen()
+    enter_product()
+    add = question_add()
+    while True:
+        if add == "y":
+            print ""
+            enter_product()
+            add = question_add()
+        elif add == "n":
+            option_n()
+            break
+        elif add != "y" or add != "n":
+            option_different()
+            add = question_add()
+
+def enter_product():
+    """This function asks a product and a price"""
     product = raw_input("Enter a product: ") #This is the item entered
     product = product.lower()
     while product.isdigit(): #While product is number makes the mext code
@@ -29,46 +52,16 @@ def option1():
             print ""
             print "Producto:", product
 
-    LIST_PRODUCT.append(product) #Adds the product to a list called "LIST_PRODUCT"
-    MY_PRODUCTS[product] = price #
+    EXISTENCE_PRODUCT.append(product) #Adds the product to a list called "EXISTENCE_PRODUCT"
+    MY_PRODUCTS[product] = price
+    print "Product added correctly"
     print ""
 
-def option_add(add):
-    """Starst a function to take a decision if we want to enter another item"""
-    if add == "y": #If we enter y calls the function option_y
-        option_y(add)
-    elif add == "n": #If we enter n calls the function option_y
-        option_n()
-    #If we enter something different of y or n calls the function option_y
-    elif add != "y" or add != "n":
-        option_different()
-
-def option_y(add):
-    """Creates a function for if we want to add another item"""
-    while add == "y":
-        option1()
-        #Ask if we want to add another item
-        add = raw_input("Do you want to add another item? Write y or n: ")
-        add = add.lower()
-        if add == "y":
-            print ""
-        LIST_PRODUCT.append(add) #If we enter y, adds it to a list
-
-        if "y" in LIST_PRODUCT or "n" in LIST_PRODUCT:
-            LIST_PRODUCT.remove(add) #If we enter y or n removes it
-        if add == "n":
-            option_n() #If we enter n call the option n
-        elif add == "y":
-            pass
-        while add != "y" and add != "n":
-            print "INSERT Y OR N!!!"
-            add = raw_input("Do you want to add another item? Write y or n: ")
-            add = add.lower()
-            if add == "y":
-                print ""
-            if add == "n":
-                option_n() #If our option is n call option_n
-                break
+def question_add():
+    """It asks if we want to add another item"""
+    add = raw_input("Do you want to add another item?: ")
+    add = add.lower()
+    return add
 
 def option_n():
     """Starts a function to go to the Main Menu"""
@@ -78,96 +71,108 @@ def option_different():
     """Starst a function if we do not enter y or n"""
     print "PLEASE ENTER \"y\" OR \"n\""
 
-#--------------------------OPCION 2--------------------------
+'''-------------------------OPCION 2-------------------------'''
 
-def my_total(buy, my_option, my_buy, my_products, total):
-    """It is a function for make the calculates"""
-    while buy != "done":
-        if buy in my_products:
-            total += my_products[buy]
-        #Ask we enter a product we want to buy
-        buy = raw_input("Do you want to buy another product?: ")
-        buy = buy.lower()
-        my_buy.append(buy) #Adds the item we entered to the list my_buy
-
-        if buy == "gold" or buy == "silver" or buy == "done":
-            my_option.append(buy)
-            if buy == "gold":
-                my_buy.remove(buy)
-            elif buy == "silver":
-                my_buy.remove(buy)
-
-        if buy != "done" and buy in my_products:
-            print "  ", buy, ("%.2f" % my_products[buy])
-
-        if "done" in my_option:
-            discount = 0
-            subtotal = 0
-            tax = total * 0.12
-            final_total = total + (tax)
-
-            #If we have silver card
-            if "silver" in my_option and "gold" not in my_option:
-                my_buy.remove(buy)
-                target = False
-                #if the user has only gold card
-                if "gold" in my_option:
-                    target = True
-                elif target == False:
-                    discount = final_total * 0.02
-                    subtotal = total + tax
-                    final_total -= discount
-
-            #If we have gold card
-            elif "gold" in my_option:
-                my_buy.remove(buy)
-                target = True
-                #If the user has gold card and don't have silver card
-                if "gold" in my_option and "silver" not in my_option:
-                    discount = final_total * 0.05
-                    subtotal = total + tax
-                    final_total -= discount
-                #If the user has gold card and silver card
-                elif "gold" in my_option and "silver" in my_option:
-                    discount = final_total * 0.05
-                    subtotal = total + tax
-                    final_total -= discount
-
-            print_bill(my_buy, LIST_PRODUCT, total, tax, subtotal, discount, final_total)
-
-        elif buy not in my_products and buy != "done" and buy != "gold" and buy != "silver":
+def option2():
+    """Makes the process of option 2"""
+    clearscreen()
+    total = 0
+    while True:
+        buy = buy_product()
+        if buy in MY_PRODUCTS:
+            print "  ", buy, ("%.2f" % MY_PRODUCTS[buy])
+            total += MY_PRODUCTS[buy]
+        elif buy not in MY_PRODUCTS and buy != "done" and buy != "gold" and buy != "silver":
+            PRODUCTS_SOLD.remove(buy)
             print "  ", "This is not registered"
 
-def print_bill(my_buy, list_product, total, tax, subtotal, discount, final_total):
+        if buy == "gold" or buy == "silver":
+            MY_OPTION.append(buy)
+            PRODUCTS_SOLD.remove(buy)
+
+        if buy == "done":
+            PRODUCTS_SOLD.remove(buy)
+            my_total(total)
+
+            if "gold" in MY_OPTION:
+                MY_OPTION.remove("gold")
+            if "silver" in MY_OPTION:
+                MY_OPTION.remove("silver")
+            del PRODUCTS_SOLD[:]
+
+            break
+    main_menu()
+
+def buy_product():
+    """It asks the product that we want to buy"""
+    buy = raw_input("Enter the product you want to buy: ")
+    buy = buy.lower()
+    PRODUCTS_SOLD.append(buy)
+    return buy
+
+def my_total(total):
+    """It is a function for make the calculates"""
+    discount = 0
+    subtotal = 0
+    tax = total * 0.12
+    subtotal = total + tax
+    final_total = total + (tax)
+
+    #If we have silver card
+    if "silver" in MY_OPTION and "gold" not in MY_OPTION:
+        target = False
+        #if the user has only gold card
+        if "gold" in MY_OPTION:
+            target = True
+        elif target == False:
+            discount = final_total * 0.02
+            subtotal = total + tax
+            final_total -= discount
+
+    #If we have gold card
+    if "gold" in MY_OPTION:
+        target = True
+        #If the user has gold card and don't have silver card
+        if "gold" in MY_OPTION and "silver" not in MY_OPTION:
+            discount = final_total * 0.05
+            subtotal = total + tax
+            final_total -= discount
+        #If the user has gold card and silver card
+        elif "gold" in MY_OPTION and "silver" in MY_OPTION:
+            discount = final_total * 0.05
+            subtotal = total + tax
+            final_total -= discount
+
+    print_bill(total, tax, discount, subtotal, final_total)
+
+def print_bill(total, tax, discount, subtotal, final_total):
     """Makes the bill"""
     print ""
-    num_times(my_buy, list_product)
+    num_times()
     print ""
     print "The Total price of the products:", ("%.2f" % total)
     print "The Tax is:", ("%.2f" % tax)
     print "The subtotal is:", ("%.2f" % subtotal)
     print "The discount is:", ("%.2f" % discount)
-    print "The Total Final is:", ("%.2f" % final_total)
+    print "The Final Total is:", ("%.2f" % final_total)
     print "Thank you for shopping with us"
     enter = raw_input("PRESS ENTER")
     if enter == "":
         clearscreen()
-        main_menu()
     while enter != "":
         enter = raw_input("PRESS ENTER")
         if enter == "":
             clearscreen()
-            main_menu()
 
-def num_times(my_buy, list_product):
+def num_times():
     """Starts a function for print the quantity of products we asked"""
     print "PRODUCTS PURCHASED"
-    for each_product in list_product: #This is for to have every item of LIST_PRODUCT
-        num_times_products_on_sale = my_buy.count(each_product)
-        if num_times_products_on_sale >= 1:
-            print num_times_products_on_sale, each_product, "a", ("%.2f" % MY_PRODUCTS[each_product])
+    for each_product in EXISTENCE_PRODUCT: #This is for to have every item of EXISTENCE_PRODUCT
+        num_products_on_sale = PRODUCTS_SOLD.count(each_product)
+        if num_products_on_sale >= 1:
+            print num_products_on_sale, each_product, "a", ("%.2f" % MY_PRODUCTS[each_product])
 
-#-------------------------MEIN MENU-------------------------
+'''------------------------MAIN MENU------------------------'''
 
 def main_menu():
     """Prints the Manin Menu"""
@@ -181,57 +186,19 @@ def main_menu():
         try:
             option = int(option)
             if option > 0 and option <= 3:
+                if option == 1:
+                    option1()
+                elif option == 2:
+                    option2()
+                elif option == 3:
+                    clearscreen()
+                    sys.exit()
                 break
         except ValueError:
             pass
         if option < 1 or option > 3:
-            print "Insert a valid option"
+            print "INSERT A VALID OPCION"
             print ""
     clearscreen()
 
-    if option == 1:
-        option1()
-        add_condition = True
-        while add_condition == True:
-            add = raw_input("Do you want to add another item? Write y or n: ")
-            add = add.lower()
-            if add == "y": #If add is equals to y breaks the cycle
-                break
-            elif add == "n": #If add is equals to n breaks the cycle
-                break
-            else: #Else print the next code
-                print "INSERT Y OR N!!!"
-        print ""
-        LIST_PRODUCT.append(add) #Add add to LIST_PRODUCT
-        if "y" in LIST_PRODUCT or "n" in LIST_PRODUCT:
-            LIST_PRODUCT.remove(add)
-        option_add(add)
-
-    elif option == 2:
-        my_buy = [] #We make a list to save the products we want
-        my_option = [] #It is a list to save the option gold/siver/done
-        buy = raw_input("Which product you want to buy?: ")
-        buy = buy.lower()
-        my_buy.append(buy)
-        if buy in MY_PRODUCTS:
-            print "  ", buy, ("%.2f" % MY_PRODUCTS[buy])
-        elif buy not in MY_PRODUCTS and buy != "done" and buy != "gold" and buy != "silver":
-            print "  ", "This is not registered"
-        while buy == "done" or buy == "gold" or buy == "silver":
-            print "First adds a product"
-            #Ask an item to buy
-            buy = raw_input("Which product you want to buy?: ")
-            buy = buy.lower()
-            if buy in MY_PRODUCTS:
-                print "  ", ("my_option%.2f" % buy, MY_PRODUCTS[buy])
-                my_buy.append(buy)
-        total = 0
-
-        my_total(buy, my_option, my_buy, MY_PRODUCTS, total)
-
-    elif option == 3: #If the input is equals to 3 makes the next code
-        pass
-
-MY_PRODUCTS = {} #This is a dictionary to have all product we have on sale
-LIST_PRODUCT = [] #This a list to have the products we have sold
 main_menu()
